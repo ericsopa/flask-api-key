8f1c05fa8248673df6c05f78b410d9d32feac420
Paul Ericson <ericson.paul@gmail.com>
2016-10-23 14:39:52 -0400

    Added HTTPS using TLS

--- goaway.py
+++ goaway.py
@@ -2,6 +2,9 @@ from flask import Flask
 app = Flask(__name__)
 from functools import wraps
 from flask import request, abort
+import ssl
+context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+context.load_cert_chain('server.crt', 'server.key')
 
 # The actual decorator function
 def require_appkey(view_function):
@@ -9,7 +12,7 @@ def require_appkey(view_function):
     # the new, post-decoration function. Note *args and **kwargs here.
     def decorated_function(*args, **kwargs):
         with open('api.key', 'r') as apikey:
-	    key=apikey.read().replace('\n', '')
+            key=apikey.read().replace('\n', '')
         if request.args.get('key') and request.args.get('key') == key:
             return view_function(*args, **kwargs)
         else:
@@ -24,3 +27,5 @@ def put_user():
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+app.run(host='127.0.0.1',port='443', debug = False/True, ssl_context=context)
